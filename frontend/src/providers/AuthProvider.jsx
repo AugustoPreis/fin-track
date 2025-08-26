e6b1d1675fbefa0d31158dcd160514bfada5002a
Augusto Tomasi <augustopreisthomasi@gmail.com>
2025-08-26 16:57:27 -0300

    adiciona provider de autenticação

---
+++ frontend/src/providers/AuthProvider.jsx
@@ -0,0 +1,61 @@
+import { useContext, createContext, useState } from 'react';
+
+const AuthContext = createContext();
+const STORAGE_USER_KEY = 'user';
+
+const parseUser = () => {
+  try {
+    const user = localStorage.getItem(STORAGE_USER_KEY);
+
+    if (user) {
+      return JSON.parse(user);
+    }
+
+    return null;
+  } catch {
+    return null;
+  }
+}
+
+export default function AuthProvider(props) {
+  const [user, setUser] = useState(parseUser);
+
+  const login = (values) => {
+    setUser(values);
+    localStorage.setItem(STORAGE_USER_KEY, JSON.stringify(values));
+  }
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem(STORAGE_USER_KEY);
+  }
+
+  const getUser = () => {
+    const user = parseUser();
+
+    return user || null;
+  }
+
+  const isAuthenticated = () => {
+    const user = parseUser();
+
+    return !!(user && user.id);
+  }
+
+  const contextValue = {
+    user,
+    login,
+    logout,
+    getUser,
+    isAuthenticated,
+  };
+
+  return (
+    <AuthContext.Provider {...props}
+      value={contextValue} />
+  );
+}
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
